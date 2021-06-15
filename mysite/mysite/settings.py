0b32520fee80850da8f1f8a147746a6653950e3b
Bryan Dixon <javawolfpack@gmail.com>
2021-06-15 16:00:30 +0000

    Cloud SQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '34.66.164.50',
+        'PORT': '5432'
     }
 }
 
